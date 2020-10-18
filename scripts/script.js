058ff9354d799a81f7d02ae2557e4035bf9bb9ef
BustaZ <pavel.zaborin@inbox.ru>
2020-10-18 15:49:47 +0500

    add mobile version

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
 const closerNavBar = document.querySelector('.closer-nav-bar');
+const borderNavBar = document.querySelector('.show-nav-bar');
 const navBar = document.querySelector('.nav-bar');
 
 closerNavBar.addEventListener('click', function(evt) {
     if (closerNavBar.classList.contains('show')) {
         closerNavBar.classList.remove('show');
+        borderNavBar.classList.remove('show');
         navBar.classList.remove('visible');
     } else {
         closerNavBar.classList.add('show');
+        borderNavBar.classList.add('show');
         navBar.classList.add('visible');
     }
 });
