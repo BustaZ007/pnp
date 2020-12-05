0899964bde956c1781730eb3bfde8e494736844b
Павел Заборин <pavelzaborin@ip-192-168-0-100.is74.loc>
2020-12-05 23:21:08 +0500

    add two links for download documents, fix JS code and use all preffics

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,17 @@
 const closerNavBar = document.querySelector('.closer-nav-bar');
 const borderNavBar = document.querySelector('.show-nav-bar');
 const navBar = document.querySelector('.nav-bar');
+const links = document.getElementsByClassName('links');
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function(evt) {
+        if (closerNavBar.classList.contains('show')) {
+            closerNavBar.classList.remove('show');
+            borderNavBar.classList.remove('show');
+            navBar.classList.remove('visible');
+        }
+    });
+}
 
 closerNavBar.addEventListener('click', function(evt) {
     if (closerNavBar.classList.contains('show')) {
